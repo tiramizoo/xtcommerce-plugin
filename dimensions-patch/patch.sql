ef94e5a561a0f88beffc56d2d627b53007c269f8
Sebastian Vollnhals <sebastian@vollnhals.info>
2012-05-16 16:06:34 +0200

    dimensions patch sql

---
+++ dimensions-patch/patch.sql
@@ -0,0 +1,5 @@
+ALTER TABLE products
+ADD products_length DECIMAL(6,2) DEFAULT '0' NOT NULL,
+ADD products_width DECIMAL(6,2) DEFAULT '0' NOT NULL,
+ADD products_height DECIMAL(6,2) DEFAULT '0' NOT NULL;
+ALTER TABLE `sessions` CHANGE `value` `value` LONGTEXT NOT NULL 
