ff8566d1a2eba505140462a33e0318d319d7a516
Sebastian Vollnhals <sebastian@vollnhals.info>
2012-05-29 19:31:38 +0200

    updated mysql patch

--- options-patch/patch.sql
+++ options-patch/patch.sql
@@ -1,5 +1,6 @@
 ALTER TABLE products
 ADD products_length DECIMAL(6,2) DEFAULT '0' NOT NULL,
 ADD products_width DECIMAL(6,2) DEFAULT '0' NOT NULL,
-ADD products_height DECIMAL(6,2) DEFAULT '0' NOT NULL;
+ADD products_height DECIMAL(6,2) DEFAULT '0' NOT NULL,
+ADD `products_instant` ENUM( 'y', 'n' ) DEFAULT 'n';
 ALTER TABLE `sessions` CHANGE `value` `value` LONGTEXT NOT NULL 
